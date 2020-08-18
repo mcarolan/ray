774e939eec6de2db9dbbbaaa3504e49e7a813fc3
Martin Carolan <mail@mcarolan.net>
2020-08-18 08:56:59 +0100

    Render a scene

--- app/Main.hs
+++ app/Main.hs
@@ -9,45 +9,79 @@ import CanvasPPM
 import System.IO
 import Transforms
 import Lighting
-
-colourAt :: Double -> Double -> Ray -> (ShapeId, Sphere) -> PointLight -> Colour
-colourAt x y ray shape light =
-  case hit (ray `intersect` shape) of
-    Just h ->
-      let
-        p = position ray (t h)
-        norm = normalAt (snd shape) p
-        eye = neg (direction ray)
-      in
-        lighting (sphereMaterial (snd shape)) light p eye norm
-    Nothing -> black
+import World
+import Camera
+import Lighting
 
 main :: IO ()
 main =
   do
-    _ <- writeFile "sphere.ppm" ppm
+    _ <- writeFile "scene.ppm" ppm
     return ()
   where
-    rayOrigin = point 0 0 (-5)
-    wallZ = 10
-    wallSize = 7
-    canvasPixels :: Double
-    canvasPixels = 500
-    pixelSize = wallSize / canvasPixels
-    half = wallSize / 2
+    floorMaterial = defaultMaterial {
+      materialColour = Colour 1 0.9 0.9,
+      materialSpecular = 0
+    }
+    floor = sphere {
+      sphereTransform = scaling 10 0.01 10,
+      sphereMaterial = floorMaterial
+    }
 
-    worldY y = half - pixelSize * y
-    worldX x = -half + pixelSize * x
-    worldPos x y = point (worldX x) (worldY y) wallZ
+    leftWall = sphere {
+       sphereTransform = translation 0 0 5 `mul`
+                         rotateY (-(pi/4)) `mul`
+                         rotateX (pi/2) `mul`
+                         scaling 10 0.01 10,
+       sphereMaterial = floorMaterial
+    }
 
-    rayAt x y = Ray rayOrigin (normalize (worldPos x y `minus` rayOrigin))
+    rightWall = sphere {
+      sphereTransform = translation 0 0 5 `mul`
+                        rotateY (pi/4) `mul`
+                        rotateX (pi/2) `mul`
+                        scaling 10 0.01 10,
+      sphereMaterial = floorMaterial
+    }
 
-    pixels = [ [ colourAt x y (rayAt x y) (shapeId, shape) light | x <- [0..(canvasPixels - 1)]] | y <- [0..(canvasPixels - 1)]]
+    middle = sphere {
+      sphereTransform = translation (-0.5) 1 0.5 `mul`
+                        shearing 1 0 0 0 0 0,
+      sphereMaterial = defaultMaterial {
+        materialColour = Colour 0.1 1 0.5,
+        materialDiffuse = 0.7,
+        materialSpecular = 0.3
+      }
+    }
 
-    c = listToCanvas pixels
-    ppm = canvasToPPM c
-    shapeId = ShapeId 0
-    material = defaultMaterial { materialColour = Colour 1 0.2 1 }
+    right = sphere {
+      sphereTransform = translation 1.5 0.5 (-0.5) `mul`
+                        scaling 0.5 0.5 0.5,
+      sphereMaterial = defaultMaterial {
+        materialColour = Colour 0.5 1 0.1,
+        materialDiffuse = 0.7,
+        materialSpecular = 0.3
+      }
+    }
+
+    left = sphere {
+      sphereTransform = translation (-1.5) 0.33 (-0.75) `mul`
+                        scaling 0.33 0.33 0.33,
+      sphereMaterial = defaultMaterial {
+        materialColour = Colour 1 0.8 0.1,
+        materialDiffuse = 0.7,
+        materialSpecular = 0.3
+      }
+    }
+
     light = PointLight white (point (-10) 10 (-10))
-    trans = shearing 1 0 0 0 0 0 `mul` rotateZ (pi / 4) `mul` scaling 0.5 1 1
-    shape = sphere { sphereMaterial = material, sphereTransform = trans }
+    empty = addLight light emptyWorld
+
+    spheres = [ floor, leftWall, rightWall, middle, right, left ]
+
+    world = foldl (flip addSphere) empty spheres
+
+    camTransform = viewTransform (point 0 1.5 (-5)) (point 0 1 0) (vector 0 1 0)
+    cam = setTransform (camera 800 600 (pi/3))  camTransform
+    canvas = render cam world
+    ppm = canvasToPPM canvas
---
+++ app/SphereMain.hs
@@ -0,0 +1,53 @@
+module SphereMain where
+
+import Quad
+import Canvas
+import Colour hiding (minus)
+import Ray
+import Data.Maybe
+import CanvasPPM
+import System.IO
+import Transforms
+import Lighting
+
+colourAt :: Double -> Double -> Ray -> (ShapeId, Sphere) -> PointLight -> Colour
+colourAt x y ray shape light =
+  case hit (ray `intersect` shape) of
+    Just h ->
+      let
+        p = position ray (t h)
+        norm = normalAt (snd shape) p
+        eye = neg (direction ray)
+      in
+        lighting (sphereMaterial (snd shape)) light p eye norm
+    Nothing -> black
+
+main :: IO ()
+main =
+  do
+    _ <- writeFile "sphere.ppm" ppm
+    return ()
+  where
+    rayOrigin = point 0 0 (-5)
+    wallZ = 10
+    wallSize = 7
+    canvasPixels :: Double
+    canvasPixels = 500
+    pixelSize = wallSize / canvasPixels
+    half = wallSize / 2
+
+    worldY y = half - pixelSize * y
+    worldX x = -half + pixelSize * x
+    worldPos x y = point (worldX x) (worldY y) wallZ
+
+    rayAt x y = Ray rayOrigin (normalize (worldPos x y `minus` rayOrigin))
+
+    pixels = [ [ colourAt x y (rayAt x y) (shapeId, shape) light | x <- [0..(canvasPixels - 1)]] | y <- [0..(canvasPixels - 1)]]
+
+    c = listToCanvas pixels
+    ppm = canvasToPPM c
+    shapeId = ShapeId 0
+    material = defaultMaterial { materialColour = Colour 1 0.2 1 }
+    light = PointLight white (point (-10) 10 (-10))
+    trans = shearing 1 0 0 0 0 0 `mul` rotateZ (pi / 4) `mul` scaling 0.5 1 1
+    shape = sphere { sphereMaterial = material, sphereTransform = trans }
